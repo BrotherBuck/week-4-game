e90a844cedeb0ce6c4a97744ab477ff730e82809
John Dispennette <jtdartworks@gmail.com>
2016-04-22 21:59:56 -0500

    week-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// Global variables
+
+//Array of Playable Characters
+var characters = ['bobafett', 'darthvader', 'hansolo', 'yoda'];
+var chosenCharacter = [];
+var chosenDefender = [];
+
+//Game counters
+var hitPoints =
+var level =
+
+//Starts th game
+function startGame(){
+
+	$("characters").click(function(){
+		alert("You selected'characters'")
+	});
+
+	console.log();
+}
+
+	document.getElementById('');
+
+	document.onClick();
+
+	document.onClick()=changeColor();
