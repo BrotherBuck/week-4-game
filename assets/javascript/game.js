d1cf9252518570d92bf751483ba8e9148c1fa1fe
John Dispennette <jtdartworks@gmail.com>
2016-04-27 14:16:28 -0500

    changed characters into button, began to form javascript for characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,26 @@
-// Global variables
-
-//Array of Playable Characters
-var characters = ['bobafett', 'darthvader', 'hansolo', 'yoda'];
-var chosenCharacter = [];
-var remainingCharacter= [];
-var chosenDefender = [];
 
 
-// character object
-
-bobafett.totalHealth: 150;
-bobafett.currentHealth: 150;
-bobafett.attack: 10;
-bobafett.counterAttack: 20;
-bobafett.image: "<img src='assets/images/bobafett.jpg'>";
-
-darthvader.totalHealth: 150;
-darthvader.currentHealth: 150;
-darthvader.attack: 10;
-darthvader.counterAttack: 20;
-darthvader.image: "<img src='assets/images/darthvader.jpg'>";
-
-hansolo.totalHealth: 150;
-hansolo.currentHealth: 150;
-hansolo.attack: 10;
-hansolo.counterAttack: 20;
-hansolo.image: "<img src='assets/images/hansolo.jpg'>";
-
-yoda.totalHealth: 150;
-yoda.currentHealth: 150;
-yoda.attack: 10;
-yoda.counterAttack: 20;
-yoda.image: "<img src='assets/images/yoda.jpg'>";
+//Starts the game
+function startGame(){
 
 
+$('#buttonBoba').on('click', function(){
+      console.log('Boba Fett');
+  }
 
-           
+      /* $('#heads').on('click', function() {
+    headsCount++;
+    $('#headsChosen').html(headsCount);
+
+    $('#guess').html('<b>Heads</b>');
+    flipThatCoin(0);
+  });
+
+
+       function multiplier (x, y, z) {
+			 return x*y*z;
+			}
+
 
 //Game counters
 var hitPoints =
@@ -43,21 +28,18 @@ var level =
 
 
 
+ /* $('#button').on('click', function(){
+      console.log(this);
+
+       $('#heads').on('click', function() {
+    headsCount++;
+    $('#headsChosen').html(headsCount);
+
+    $('#guess').html('<b>Heads</b>');
+    flipThatCoin(0);
+  });
 
 
-        
-
-Starts the game
-function startGame(){
-
-	$("characters").click(function(){
-	});
-
-	console.log();
-   }
-
-	document.getElementById('');
-
-	document.onClick();
-
-	document.onClick()=changeColor(); 
+       function multiplier (x, y, z) {
+			 return x*y*z;
+			}
