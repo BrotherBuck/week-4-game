3a83eeabbe03610b128df4877a2f206a0934ff12
John Dispennette <jtdartworks@gmail.com>
2016-04-27 05:23:25 -0500

    working on javascript object function and front end layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,42 +3,61 @@
 //Array of Playable Characters
 var characters = ['bobafett', 'darthvader', 'hansolo', 'yoda'];
 var chosenCharacter = [];
+var remainingCharacter= [];
 var chosenDefender = [];
+
+
+// character object
+
+bobafett.totalHealth: 150;
+bobafett.currentHealth: 150;
+bobafett.attack: 10;
+bobafett.counterAttack: 20;
+bobafett.image: "<img src='assets/images/bobafett.jpg'>";
+
+darthvader.totalHealth: 150;
+darthvader.currentHealth: 150;
+darthvader.attack: 10;
+darthvader.counterAttack: 20;
+darthvader.image: "<img src='assets/images/darthvader.jpg'>";
+
+hansolo.totalHealth: 150;
+hansolo.currentHealth: 150;
+hansolo.attack: 10;
+hansolo.counterAttack: 20;
+hansolo.image: "<img src='assets/images/hansolo.jpg'>";
+
+yoda.totalHealth: 150;
+yoda.currentHealth: 150;
+yoda.attack: 10;
+yoda.counterAttack: 20;
+yoda.image: "<img src='assets/images/yoda.jpg'>";
+
+
+
+           
 
 //Game counters
 var hitPoints =
 var level =
 
 
-// character object
-
-	var character id = 
-		
-            bobaFett: {
-            name: "Boba Fett",
-            totalHealth: 150,
-            currentHealth: 150,
-            attack: 10,
-            counterAttack: 20,
-            
-            image: "<img src='assets/images/bobafett.jpg'>",
-            },
 
 
 
+        
 
-//Starts th game
+Starts the game
 function startGame(){
 
 	$("characters").click(function(){
-		alert("You selected'characters'")
 	});
 
 	console.log();
-}
+   }
 
 	document.getElementById('');
 
 	document.onClick();
 
-	document.onClick()=changeColor();
+	document.onClick()=changeColor(); 
